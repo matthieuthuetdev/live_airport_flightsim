310317c56e8fe3cb4c82b2ee235316deac74b424
matthieuthuetdev <mthuet.pro@gmail.com>
2023-11-26 20:42:16 +0100

    fix: sufix GND is don't working

--- app/function.php
+++ app/function.php
@@ -54,30 +54,37 @@ function load_json_file($loader, $time)
  */
 
 
- function find_airport($airport_code, $json)
- {
-     $airport_to_find_TWR = strtoupper(trim($airport_code)) . "_TWR";
-     $airport_to_find_APP = strtoupper(trim($airport_code)) . "_APP";
-     $airport_index_TWR = null;
-     $airport_index_APP = null;
- 
-     foreach ($json["clients"]["atcs"] as $index => $current_airport) {
-         if (is_int(strpos($current_airport["callsign"], $airport_to_find_TWR))) {
-             $airport_index_TWR = $index;
-             break;
-         }
-         
-         if (is_int(strpos($current_airport["callsign"], $airport_to_find_APP)) && $airport_index_TWR === null) {
-             $airport_index_APP = $index;
-         }
-     }
- 
-     if ($airport_index_TWR !== null) {
-         return $airport_index_TWR;
-     } else {
-         return $airport_index_APP;
-     }
- }
+function find_airport($airport_code, $json)
+{
+    $airport_to_find_TWR = strtoupper(trim($airport_code)) . "_TWR";
+    $airport_to_find_APP = strtoupper(trim($airport_code)) . "_APP";
+    $airport_to_find_GND = strtoupper(trim($airport_code)) . "_GND";
+    $airport_index_TWR = null;
+    $airport_index_APP = null;
+    $airport_index_GND = null;
+
+    foreach ($json["clients"]["atcs"] as $index => $current_airport) {
+        if (is_int(strpos($current_airport["callsign"], $airport_to_find_TWR))) {
+            $airport_index_TWR = $index;
+            break;
+        }
+
+        if (is_int(strpos($current_airport["callsign"], $airport_to_find_APP)) && $airport_index_TWR === null) {
+            $airport_index_APP = $index;
+        }
+        if (is_int(strpos($current_airport["callsign"], $airport_to_find_GND)) && $airport_index_TWR === null) {
+            $airport_index_GND = $index;
+        }
+    }
+
+    if ($airport_index_TWR !== null) {
+        return $airport_index_TWR;
+    } else if ($airport_index_APP !== null) {
+        return $airport_index_APP;
+    } else if ($airport_index_GND !== null) {
+        return $airport_index_GND;
+    }
+}
 /**
  * la fonction status renvoi le texte à afficher à côté du formulaire,
  * si l'aéroport est en ligne le texte connecté doit être afficher et sinon le texte déconnecté doit être afficher.
