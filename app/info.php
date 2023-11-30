607acffcd8146ae6dd8e5c1f8e192227b00a80a0
matthieuthuetdev <mthuet.pro@gmail.com>
2023-11-30 15:37:58 +0100

    chore: add APP and GND information in the first information table

--- app/info.php
+++ app/info.php
@@ -1,9 +1,21 @@
 <?php
-
+extract($airport_station_index);
 $information = $json["clients"]["atcs"][$airport_index]["atis"]["revision"];
 $airport_name = $json["clients"]["atcs"][$airport_index]["atis"]["lines"][1];
 $airport_code = $json["clients"]["atcs"][$airport_index]["callsign"];
 $atis = $json["clients"]["atcs"][$airport_index]["atis"]["lines"];
+if($TWR_index !== null){
+$airport_name_TWR = $json["clients"]["atcs"][$TWR_index]["atis"]["lines"][1];
+$airport_code_TWR = $json["clients"]["atcs"][$TWR_index]["callsign"];
+}
+if($APP_index !== null){
+$airport_name_APP = $json["clients"]["atcs"][$APP_index]["atis"]["lines"][1];
+$airport_code_APP = $json["clients"]["atcs"][$APP_index]["callsign"];
+}
+if($GND_index !== null){
+$airport_name_GND = $json["clients"]["atcs"][$GND_index]["atis"]["lines"][1];
+$airport_code_GND = $json["clients"]["atcs"][$GND_index]["callsign"];
+}
 ?>
 <p>
 <table class="info big_screan">
@@ -13,30 +25,18 @@ $atis = $json["clients"]["atcs"][$airport_index]["atis"]["lines"];
         <th class="info">Statu</th>
     </tr>
     <tr>
-        <td class="info"><?php echo $airport_code; ?></td>
-        <td class="info"><?php echo $airport_name; ?></td>
-        <td class="info"><?php echo $information; ?></td>
-        <td class="info"><?php echo $update_hour_airport . " UTC"; ?></td>
-    </tr>
-    <tr>
         <td class="info"><?php echo $airport_station_index["airport_to_find_TWR"] ?></td>
-        <td class="info"></td>
-        <td class="info"></td>
-        <td class="info"></td>
+        <td class="info"><?php echo isset($airport_name_TWR) ? $airport_name_TWR : "" ?></td>
         <td class="info"><?php echo ($oneline_TWR == true) ? "<span class='online_station'>Connecté</span>" : "<span class='offline_station'>Déconnecté</span>" ?></td>
     </tr>
     <tr>
         <td class="info"><?php echo $airport_station_index["airport_to_find_APP"] ?></td>
-        <td class="info"></td>
-        <td class="info"></td>
-        <td class="info"></td>
+        <td class="info"><?php echo isset($airport_name_APP) ? $airport_name_APP : "" ?></td>
         <td class="info"><?php echo ($oneline_APP == true) ? "<span class='online_station'>Connecté</span>" : "<span class='offline_station'>Déconnecté</span>" ?></td>
     </tr>
     <tr>
         <td class="info"><?php echo $airport_station_index["airport_to_find_GND"] ?></td>
-        <td class="info"></td>
-        <td class="info"></td>
-        <td class="info"></td>
+        <td class="info"><?php echo isset($airport_name_GND) ? $airport_name_GND : "" ?></td>
         <td class="info"><?php echo ($oneline_GND == true) ? "<span class='online_station'>Connecté</span>" : "<span class='offline_station'>Déconnecté</span>" ?></td>
     </tr>
 </table>
