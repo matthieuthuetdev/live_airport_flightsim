7ebecd947ba864843a14005a796fe6d68273e6b4
matthieuthuetdev <mthuet.pro@gmail.com>
2023-12-04 16:52:15 +0100

    chore: remove the name column

--- app/info.php
+++ app/info.php
@@ -17,30 +17,27 @@ if ($GND_index !== null) {
     $airport_name_GND = $json["clients"]["atcs"][$GND_index]["atis"]["lines"][1];
     $airport_connection_time_GND = gmdate("h:i:s", $json["clients"]["atcs"][$GND_index]["time"]);
 }
+var_dump($airport_connection_time_TWR)
 ?>
 <p>
 <table class="info ">
     <tr>
         <th class="info">Code ICAO</th>
-        <th class="info">Nom</th>
         <th class="info">Statut</th>
         <th class="info">Temps de connexion</th>
     </tr>
     <tr>
         <td class="info"><?php echo $airport_station_index["airport_to_find_TWR"] ?></td>
-        <td class="info"><?php echo isset($airport_name_TWR) ? $airport_name_TWR : "" ?></td>
         <td class="info"><?php echo ($oneline_TWR == true) ? "<span class='online_station'>Connecté</span>" : "<span class='offline_station'>Déconnecté</span>" ?></td>
         <td class="info"><?php echo isset($airport_connection_time_TWR) ? $airport_connection_time_TWR : "" ?></td>
     </tr>
     <tr>
         <td class="info"><?php echo $airport_station_index["airport_to_find_APP"] ?></td>
-        <td class="info"><?php echo isset($airport_name_APP) ? $airport_name_APP : "" ?></td>
         <td class="info"><?php echo ($oneline_APP == true) ? "<span class='online_station'>Connecté</span>" : "<span class='offline_station'>Déconnecté</span>" ?></td>
         <td class="info"><?php echo isset($airport_connection_time_APP) ? $airport_connection_time_APP : "" ?></td>
     </tr>
     <tr>
         <td class="info"><?php echo $airport_station_index["airport_to_find_GND"] ?></td>
-        <td class="info"><?php echo isset($airport_name_GND) ? $airport_name_GND : "" ?></td>
         <td class="info"><?php echo ($oneline_GND == true) ? "<span class='online_station'>Connecté</span>" : "<span class='offline_station'>Déconnecté</span>" ?></td>
         <td class="info"><?php echo isset($airport_connection_time_GND) ? $airport_connection_time_GND : "" ?></td>
     </tr>
